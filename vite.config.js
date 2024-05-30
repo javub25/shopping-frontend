8d0c59a1cc6069a8f525e785c28196d7ba14b6b8
javub25 <ju231415sad@gmail.com>
2024-05-30 19:14:03 +0200

    Adding alias

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import envCompatible from 'vite-plugin-env-compatible';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(
@@ -9,4 +10,36 @@ export default defineConfig(
       react(), 
       envCompatible(), // Load environment variables from .env files
     ],
+    resolve: {
+      alias: [
+        {
+          find: '@services',
+          replacement: path.resolve(path.join(__dirname, '/src/services')),
+        },
+        {
+          find: '@components',
+          replacement: path.resolve(path.join(__dirname, '/src/components')),
+        },
+        {
+          find: '@assets',
+          replacement: path.resolve(path.join(__dirname, '/src/assets')),
+        },
+        {
+          find: '@pages',
+          replacement: path.resolve(path.join(__dirname, '/src/pages'))
+        },
+        {
+          find: '@contexts',
+          replacement: path.resolve(path.join(__dirname, '/src/contexts'))
+        },
+        {
+          find: '@hooks',
+          replacement: path.resolve(path.join(__dirname, '/src/hooks'))
+        },
+        {
+          find: '@utilities',
+          replacement: path.resolve(path.join(__dirname, '/src/utilities'))
+        }
+      ]
+    }
 })
